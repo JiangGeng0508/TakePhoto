cd15c6242c7a5e973f86ec71cde20c6ada905051
ZinC0508 <18335432463@163.com>
2025-09-16 19:37:31 +0800

    Catch

--- Scripts/Cursor.cs
+++ Scripts/Cursor.cs
@@ -1,6 +1,43 @@
 using Godot;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using TakePhoto.Scripts;
 
 public partial class Cursor : Node2D
 {
+    private List<VampArea> _detectVamps = [];
+    private Label _nameList;
+
+    public override void _Ready()
+    {
+        _nameList = GetNode<Label>("NameList");
+    }
+
+    public void OnAreaEntered(Area2D area)
+    {
+        if (area is VampArea vamp)
+        {
+            _detectVamps.Add(vamp);
+            RefreshNameList();
+        }
+    }
+
+    public void OnAreaExited(Area2D area)
+    {
+        if (area is VampArea vamp && _detectVamps.Contains(vamp))
+        {
+            _detectVamps.Remove(vamp);
+            RefreshNameList();
+        }
+    }
+
+    public void RefreshNameList()
+    {
+        _nameList.Text = "";
+        foreach (var vampArea in _detectVamps)
+        {
+            _nameList.Text += $"{vampArea.Name}\n";
+        }
+    }
 }
